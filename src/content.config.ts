14a060459350848a125ea1a68c3385c267841af3
BadrChoubai <choubai.badr@protonmail.com>
2025-11-14 14:54:40 -0700

    feat: content MVP

---
+++ src/content.config.ts
@@ -0,0 +1,20 @@
+// 1. Import utilities from `astro:content`
+import { defineCollection, z } from "astro:content";
+
+// 2. Import loader(s)
+import { glob, file } from "astro/loaders";
+
+// 3. Define your collection(s)
+const blog = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./src/content/blog" }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    published: z.coerce.date(),
+    updated: z.coerce.date().optional(),
+    tags: z.array(z.string()),
+  }),
+});
+
+// 4. Export a single `collections` object to register your collection(s)
+export const collections = { blog };
